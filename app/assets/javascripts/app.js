368f761df5d6ca2297328d88d5f05a7827a6a56c
RyanKim01 <kyhekm@hotmail.com>
2016-04-08 18:13:27 -0700

    Added nav folder and devise gem

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,16 +1,20 @@
-angular.module('flapperNews', ['ui.router', 'templates'])
-  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: 'home/_home.html',
-        controller: 'MainCtrl'
-      })
-      .state('posts', {
-        url: '/posts/{id}',
-        templateUrl: 'posts/_posts.html',
-        controller: 'PostsCtrl'
-      });
+angular.module('flapperNews', ['ui.router','templates','Devise'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('home');
-  }]);
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'home/_home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: 'posts/_posts.html',
+			controller: 'PostsCtrl'
+		});
+
+		$urlRouterProvider.otherwise('home');
+}]);
--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -1,24 +1,29 @@
-.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+angular.module('flapperNews')
+.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts){
 
-		resolve: {
-			postPromise: ['posts', function(posts){
-				return posts.getAll();
-			}]
-		};
+	resolve: {
+		postPromise: ['posts', function(posts){
+			return posts.getAll();
+		}]
+	};
 
-		$scope.posts = posts.posts;
+	$scope.posts = posts.posts;
 
-    $scope.addPost = function() {
-      if (!$scope.title || $scope.title === "") { return; }
-			posts.create({
-				title: $scope.title,
-				link: $scope.link
-			});
-      $scope.title = "";
-      $scope.link = "";
-    };
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === ''){ return; }
+		posts.create({
+			title: $scope.title,
+			link: $scope.link,
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
 
-    $scope.incrementUpvotes = function(post) {
-      posts.upvote(post);
-    };
-  }]);
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1;
+	};
+
+}]);
